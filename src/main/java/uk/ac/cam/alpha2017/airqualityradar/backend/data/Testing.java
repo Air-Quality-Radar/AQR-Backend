f1bbee2a953180e78b3edd35907b0ef4f8c7a80a
JirKalhotka <jl887@cam.ac.uk>
2017-02-28 12:50:23 +0000

    Added class I've been using for testing so others can hack

--- src/main/java/uk/ac/cam/alpha2017/airqualityradar/backend/data/Testing.java
+++ src/main/java/uk/ac/cam/alpha2017/airqualityradar/backend/data/Testing.java
@@ -3,23 +3,62 @@ package uk.ac.cam.alpha2017.airqualityradar.backend.data;
 // Include the following imports to use table APIs
 
 import com.microsoft.azure.storage.CloudStorageAccount;
-import com.microsoft.azure.storage.table.*;
+import com.microsoft.azure.storage.StorageException;
+import com.microsoft.azure.storage.table.CloudTable;
+import com.microsoft.azure.storage.table.CloudTableClient;
+import com.microsoft.azure.storage.table.TableQuery;
 import com.microsoft.azure.storage.table.TableQuery.QueryComparisons;
 import uk.ac.cam.alpha2017.airqualityradar.backend.data.entities.DataRowEntity;
+import uk.ac.cam.alpha2017.airqualityradar.backend.models.AirDataPoint;
+import uk.ac.cam.alpha2017.airqualityradar.backend.models.DataPoint;
+import uk.ac.cam.alpha2017.airqualityradar.backend.models.Location;
+import uk.ac.cam.alpha2017.airqualityradar.backend.models.measurements.NumberMeasurement;
 
+import java.net.URISyntaxException;
+import java.security.InvalidKeyException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 import static uk.ac.cam.alpha2017.airqualityradar.backend.data.StorageConnectionInfo.storageConnectionString;
 
 public class Testing {
     // Define the connection-string with your values.
+    public static Calendar toCalendar(String dateString) throws ParseException {
+        DateFormat formatter = new SimpleDateFormat("yyyyMMdd HH:mm:ss");
+        Date date = formatter.parse(dateString);
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        return cal;
+    }
 
+    public static void main(String[] args) throws ParseException, StorageException, InvalidKeyException, URISyntaxException {
+        HistoricalDataProvider hdp = new HistoricalDataProvider();
+        Calendar cal1 = toCalendar("20160109 16:00:00");
+        Calendar cal2 = toCalendar("20160109 16:00:00");
+        List<Calendar> calList = new LinkedList<>();
+        calList.add(cal1);calList.add(cal2);
+        Location loc1 = new Location(52214239.0, 136262.0);
+        Location loc2 = new Location(52214239, 136262);
+        List<Location> locList = new LinkedList<>();
+        locList.add(loc1);locList.add(loc2);
+        for (DataPoint x : hdp.getDataPoints(calList, locList)) {
 
-    public static void main(String[] args) {
+            AirDataPoint adp = x.getAir();
+            System.out.printf("%s,%s,%s\n", printNumMes(adp.getNox()),printNumMes(adp.getPm10()),printNumMes(adp.getPm25()));
+        }
         listTables();
-        Calendar calendar = new Calendar(new Date(""))
-//        getEntities();
+//        Calendar calendar = new Calendar(new Date(""))
+        getEntities();
+    }
+
+    public static String printNumMes (NumberMeasurement numMes) {
+        if (numMes==null) {return "null";}
+        else return String.format("%s",numMes.getValue());
     }
 
     public static void listTables() {
@@ -76,7 +115,16 @@ public class Testing {
             // Loop through the results, displaying information about the entity.
             // FAILS
             for (DataRowEntity entity : entityIterable) {
-                System.out.println(entity.getRowKey());
+                System.out.println("The year is " + entity.getYear());
+                System.out.println("The minute is " + entity.getMinutes());
+                System.out.println("The day is " + entity.getDays());
+                System.out.println("The longitude is " + entity.getLongitude());
+                System.out.println("The latitude is " + entity.getLatitude());
+                System.out.println("Nox is " + entity.getNOx());
+                System.out.println("PM10 is " + entity.getPM10());
+                System.out.println("PM25 is " + entity.getPM25());
+
+                System.out.println(entity.getRowKey() + " lol " + String.format("%s,%s,%s", entity.getNOx(), entity.getPM10(), entity.getPM25()));
             }
         } catch (Exception e) {
             // Output the stack trace.
