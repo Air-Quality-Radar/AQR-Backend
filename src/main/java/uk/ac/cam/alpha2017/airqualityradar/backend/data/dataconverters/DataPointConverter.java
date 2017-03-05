e11e7d192e43ffdfe462368fa9903c9e81d548aa
JirKalhotka <jl887@cam.ac.uk>
2017-03-05 12:48:27 +0000

    RC: Refactored DataPointConverter

--- src/main/java/uk/ac/cam/alpha2017/airqualityradar/backend/data/dataconverters/DataPointConverter.java
+++ src/main/java/uk/ac/cam/alpha2017/airqualityradar/backend/data/dataconverters/DataPointConverter.java
@@ -29,28 +29,35 @@ public class DataPointConverter {
         }
 
         Location location = new Location(airDataEntity.getLatitude().doubleValue() / locationScalingFactor, airDataEntity.getLongitude().doubleValue() / locationScalingFactor);
+        AirDataPoint airDataPoint = convertAirToDataPoint(airDataEntity);
+        WeatherDataPoint weatherDataPoint;
 
+        if (weatherDataEntity == null) {
+            weatherDataPoint = null;
+        }
+        else {
+            weatherDataPoint = convertWeatherToDataPoint(weatherDataEntity);
+        }
+
+        return new DataPoint(calendar, location, airDataPoint, weatherDataPoint);
+    }
+
+    private AirDataPoint convertAirToDataPoint (AirDataEntity airDataEntity) {
         NOxMeasurement NOx = airDataEntity.getNOx().equals("") ? null : new NOxMeasurement(Double.parseDouble(airDataEntity.getNOx()));
         PM10Measurement PM10 = airDataEntity.getPM10().equals("") ? null : new PM10Measurement(Double.parseDouble(airDataEntity.getPM10()));
         PM25Measurement PM25 = airDataEntity.getPM25().equals("") ? null : new PM25Measurement(Double.parseDouble(airDataEntity.getPM25()));
 
-        AirDataPoint airDataPoint = new AirDataPoint(NOx, PM10, PM25);
+        return new AirDataPoint(NOx, PM10, PM25);
+    }
 
-        if (weatherDataEntity == null) {
-            return new DataPoint(calendar, location, airDataPoint, null);
-        }
+    private WeatherDataPoint convertWeatherToDataPoint(WeatherDataEntity weatherDataEntity) {
+        TemperatureMeasurement temperatureMeasurement = weatherDataEntity.getTemperature().equals("") ? null : new TemperatureMeasurement(Double.parseDouble(weatherDataEntity.getTemperature()));
+        HumidityMeasurement humidityMeasurement = weatherDataEntity.getHumidity().equals("") ? null : new HumidityMeasurement(Double.parseDouble(weatherDataEntity.getHumidity()));
+        WindDirectionMeasurement windDirectionMeasurement = weatherDataEntity.getWindDirection().equals("") ? null : new WindDirectionMeasurement(weatherDataEntity.getWindDirection());
+        WindSpeedMeasurement windSpeedMeasurement = weatherDataEntity.getWindSpeed().equals("") ? null : new WindSpeedMeasurement(Double.parseDouble(weatherDataEntity.getWindSpeed()));
+        PressureMeasurement pressureMeasurement = weatherDataEntity.getPressure().equals("") ? null : new PressureMeasurement(Double.parseDouble(weatherDataEntity.getPressure()));
+        RainfallMeasurement rainfallMeasurement = weatherDataEntity.getRainfallInPastHour().equals("") ? null : new RainfallMeasurement(Double.parseDouble(weatherDataEntity.getRainfallInPastHour()));
 
-        else {
-            TemperatureMeasurement temperatureMeasurement = weatherDataEntity.getTemperature().equals("") ? null : new TemperatureMeasurement(Double.parseDouble(weatherDataEntity.getTemperature()));
-            HumidityMeasurement humidityMeasurement = weatherDataEntity.getHumidity().equals("") ? null : new HumidityMeasurement(Double.parseDouble(weatherDataEntity.getHumidity()));
-            WindDirectionMeasurement windDirectionMeasurement = weatherDataEntity.getWindDirection().equals("") ? null : new WindDirectionMeasurement(weatherDataEntity.getWindDirection());
-            WindSpeedMeasurement windSpeedMeasurement = weatherDataEntity.getWindSpeed().equals("") ? null : new WindSpeedMeasurement(Double.parseDouble(weatherDataEntity.getWindSpeed()));
-            PressureMeasurement pressureMeasurement = weatherDataEntity.getPressure().equals("") ? null : new PressureMeasurement(Double.parseDouble(weatherDataEntity.getPressure()));
-            RainfallMeasurement rainfallMeasurement = weatherDataEntity.getRainfallInPastHour().equals("") ? null : new RainfallMeasurement(Double.parseDouble(weatherDataEntity.getRainfallInPastHour()));
-
-            WeatherDataPoint weatherDataPoint = new WeatherDataPoint(temperatureMeasurement, humidityMeasurement, windDirectionMeasurement, windSpeedMeasurement, pressureMeasurement, rainfallMeasurement);
-
-            return new DataPoint(calendar, location, airDataPoint, weatherDataPoint);
-        }
+        return new WeatherDataPoint(temperatureMeasurement, humidityMeasurement, windDirectionMeasurement, windSpeedMeasurement, pressureMeasurement, rainfallMeasurement);
     }
 }
